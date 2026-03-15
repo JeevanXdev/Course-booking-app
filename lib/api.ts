d5d26578a7d77710c1c038822e9cde1542a5eac3
JeevanXdev <jeevanpradhan347@gmail.com>
2026-03-15 13:56:08 +0530

    Updating the api.ts file so that the token will attach automatically on login request

---
+++ lib/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const api = axios.create({
+  baseURL: API_URL,
+});
+
+// Attach token automatically on every request
+api.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("access");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+});
+
+export const setAuthToken = (token: string | null) => {
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
+};
